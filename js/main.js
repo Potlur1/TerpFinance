84f74f468aca8456c243ed0fb6fa3c2f3771a5fc
Iyebe <ivanval0271@gmail.com>
2026-02-27 23:13:20 -0500

    tab 3 and tab4

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function showNotification() {
+    document.getElementById("notif").style.display = "block";
+}
+
+function closeNotification() {
+    document.getElementById("notif").style.display = "none";
+}
